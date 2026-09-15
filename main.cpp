0d11bdb571af53318aac4477577b0c8cb6b2eb04
Atul Pandey <atulpandey.iitr@gmail.com>
2026-09-15 12:55:04 +0530

    Initialize project structure

---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+
+int main() {
+    std::cout << "FUSE_Factory_Simulation_and_Optimization_Environment" << std::endl;
+    std::cout << "Project initialized successfully." << std::endl;
+
+    return 0;
+}
